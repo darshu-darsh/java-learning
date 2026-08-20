f03cbdab69699431bceede236b18c63bc0ed8ceb
darshu-darsh <saidarshanmt1810@gmail.com>
2026-08-20 10:28:23 +0530

    Day 2: arithmetic operators

---
+++ day2.java
@@ -0,0 +1,12 @@
+public class day2 {
+    public static void main(String[] args) {
+int a = 25;
+int b = 14;
+
+System.out.println(a + b);
+System.out.println(a - b);
+System.out.println(a * b);
+System.out.println(a / b);
+
+    }
+}
