d6848f609658dff246bd0ca9dce44b70505e3514
darshu-darsh <saidarshanmt1810@gmail.com>
2026-08-20 11:24:07 +0530

    scanner concept

---
+++ userInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class userInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        System.out.println("you entered: " + age);
+
+    }
+    
+}
