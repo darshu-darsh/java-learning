0ec11cacdc77fb3630c266c1c6e0909e7fb7d9aa
darshu-darsh <saidarshanmt1810@gmail.com>
2026-08-21 13:53:05 +0530

    merge remote changes

---
+++ day2.java
@@ -0,0 +1,13 @@
+public class day2 {
+    public static void main(String[] args) {
+int a = 25;
+int b = 14;
+
+System.out.println(a + b);
+System.out.println(a - b);
+System.out.println(a * b);
+System.out.println(a / b);
+System.out.println( a % b);
+
+    }
+}
